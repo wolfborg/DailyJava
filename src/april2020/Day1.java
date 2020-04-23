bf093567682a0f7810d225753f2dc9a6797aff78
Derek Chaplin <wolfborg@users.noreply.github.com>
2020-04-22 22:13:40 -0400

    Day1 solution & assert fix

--- src/april2020/Day1.java
+++ src/april2020/Day1.java
@@ -20,19 +20,97 @@ public class Day1 {
 	}
 	
 	public static List<String> logSorting(int numLines, List<String> loglines) {
-		// Solution here
-		return new ArrayList<String>();
+		// Initialize empty lists for newLog and sorting
+		List<String> newLog = new ArrayList<String>();
+		List<String[]> wordLog = new ArrayList<String[]>();
+		List<String> intLog = new ArrayList<String>();
+		
+		// Separate word and int logs
+		for (String log : loglines) {
+			try {
+				// Add integers to intLog
+				Integer.parseInt(log.split(" ")[1]);
+				intLog.add(log);
+			} catch(Exception e) {
+				// Add words to wordLog
+				// Splits words and ids for sorting
+				wordLog.add(new String[] {
+					log.substring(log.indexOf(" ") + 1),
+					log.split(" ")[0]
+				});
+			}
+		}
+		
+		// Sort word logs alphabetically, ignoring identifier
+		Collections.sort(wordLog, new Comparator<String[]>() {
+		    public int compare(String[] a, String[] b) {
+				// Ties sorted by identifier
+				if (a[0].equals(b[0])) { return a[1].compareTo(b[1]); }
+				// Sort list alphabetically
+		        return a[0].compareTo(b[0]);
+		    }
+		});
+		
+		// Add word logs
+		for (String[] log : wordLog) {
+			newLog.add(log[1] + " " + log[0]);
+		}
+		
+		// Add integer logs
+		for (String log : intLog) {
+			newLog.add(log);
+		}
+		
+		// Return the reordered log
+		return newLog;
 	}
 	
 	public static void problem1() {
 		// Run problem tests
-		List<String> test1input = new ArrayList<String>(Arrays.asList("a1 9 2 3 1", "g1 act car", "zo4 4 7", "ab1 off key dog", "a8 act zoo"));
-		List<String> test1expected = new ArrayList<String>(Arrays.asList("g1 act car", "a8 act zoo", "ab1 off key dog", "a1 9 2 3 1", "zo4 4 7"));
+		System.out.println("Test 1:");
+		List<String> test1input = new ArrayList<String>(Arrays.asList(
+				"a1 9 2 3 1",
+				"g1 act car",
+				"zo4 4 7",
+				"ab1 off key dog",
+				"a8 act zoo"
+			));
+		List<String> test1expected = new ArrayList<String>(Arrays.asList(
+				"g1 act car",
+				"a8 act zoo",
+				"ab1 off key dog",
+				"a1 9 2 3 1",
+				"zo4 4 7"
+			));
 		assertProblem1(test1input, test1expected);
+		
+		
+		System.out.println("Test 2:");
+		List<String> test2input = new ArrayList<String>(Arrays.asList(
+				"zz2 act car",
+				"a1 9 2 3 1",
+				"g1 act car",
+				"a2 act car",
+				"zo4 4 7",
+				"ab1 off key dog",
+				"b7 act car",
+				"a8 act zoo"
+			));
+		List<String> test2expected = new ArrayList<String>(Arrays.asList(
+				"a2 act car",
+				"b7 act car",
+				"g1 act car",
+				"zz2 act car",
+				"a8 act zoo",
+				"ab1 off key dog",
+				"a1 9 2 3 1",
+				"zo4 4 7"
+			));
+		assertProblem1(test2input, test2expected);
 	}
 	
 	public static void assertProblem1(List<String> input, List<String> expected) {
-		System.out.println("Assessing problem1:\n");
+		System.out.println("Assessing problem 1:\n");
 		
 		List<String> output = logSorting(input.size(), input);
 		
@@ -40,10 +118,11 @@ public class Day1 {
 		System.out.println("Output:"); printLogs(output); System.out.println();
 		System.out.println("Expected:"); printLogs(expected); System.out.println();
 		
-		if (output == expected) { System.out.println("Correct"); }
+		System.out.println("Result:");
+		if (output.equals(expected)) { System.out.println("Correct"); }
 		else { System.out.println("Incorrect"); }
 		
-		System.out.println();
+		System.out.println("\n===========================\n");
 	}
 	
 	public static void printLogs(List<String> loglines) {
