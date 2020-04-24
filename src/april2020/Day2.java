3f6341b2be6a01ea306dd08fc1ebd3a0a5951a25
Derek Chaplin <wolfborg@users.noreply.github.com>
2020-04-24 02:23:03 -0400

    Solution 1 with a while loop

--- src/april2020/Day2.java
+++ src/april2020/Day2.java
@@ -21,18 +21,46 @@ package april2020;
 
 public class Day2 {
 	/************************************************************************
-	 * Solution:
+	 * Solution 1:
 	 * 
+	 * Pretty simple, we set a while loop to keep increasing the population
+	 * annually with the parameters given until the targer is reached and we
+	 * return the number of times we ran the loop.
 	 * 
+	 * The main things to pay attention to is making sure the formula is set
+	 * correctly based on the prompt and the percentage treatment. The percent
+	 * actually needs to be converted to the percent after input. So 2 needs
+	 * to be turned into 0.02, which you do by just dividing it by 100.
 	 * 
-	 * Time complexity: 
+	 * There is a problem with it though where if the population doesn't grow
+	 * then you end up in an infinite loop. So I've added a quick check to
+	 * prevent that.
+	 * 
+	 * Time complexity: O(1)
 	 ************************************************************************/
 	
 	public static int populationGrowth(int p0, double percent, int aug, int p) {
+		int n = 0;
+		int p1 = 0;
 		
-		// Solution here
+		System.out.println("Initial: " + p0);
+		System.out.println("Target:  " + p);
+		System.out.println();
 		
-        return 0;
+		// Increase population until target
+		while (p0 < p) {
+			// Population increase calculation
+			p1 = (int) (p0 + (p0 * (percent/100)) + aug);
+			// Check for decrease
+			if (p0 > p1) { return -1; }
+			p0 = p1; // Apply increase
+			n++; // Increment year count
+			//System.out.println("Growth " + n + ": " + p0);
+		}
+		
+		//System.out.println();
+		
+        return n;
     }
 	
 	// Run problem tests
@@ -43,10 +71,11 @@ public class Day2 {
 	public static void problemTests() {
 		System.out.println("Running Tests:");
 		System.out.println("===========================\n");
-		test(populationGrowth(1000, 0.02, 50, 1200), 3);
+		test(populationGrowth(1000, 2, 50, 1200), 3);
 		test(populationGrowth(1500, 5, 100, 5000),15);
 		test(populationGrowth(1500000, 2.5, 10000, 2000000), 10);
 		test(populationGrowth(1500000, 0.25, 1000, 2000000), 94);
+		test(populationGrowth(100, 0.25, -5, 2000000), -1);
 	}
 	
 	public static void test(int result, int expect) {
