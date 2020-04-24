170529a290bb216a12c8d5cd65beaf1657e52a02
Derek Chaplin <wolfborg@users.noreply.github.com>
2020-04-24 15:14:51 -0400

    Day3 setup

--- src/april2020/Day2.java
+++ src/april2020/Day2.java
@@ -3,11 +3,12 @@ package april2020;
 /*******************************************************************
  * Problem: Population Growth
  * Date: April 23nd, 2020
- * In a small town the population is p0 = 1000 at the beginning of a year.
- * The population regularly increases by 2 percent per year and moreover
- * 50 new inhabitants per year come to live in the town. How many years
- * does the town need to see its population greater or equal to p = 1200
- * inhabitants?
+ * 
+ * In a small town the population is p0 = 1000 at the beginning of
+ * a year. The population regularly increases by 2 percent per year
+ * and moreover 50 new inhabitants per year come to live in the town.
+ * How many years does the town need to see its population greater
+ * or equal to p = 1200 inhabitants?
  * 
  * Input:
  * - Initial population
---
+++ src/april2020/Day3.java
@@ -0,0 +1,45 @@
+package april2020;
+
+/*******************************************************************
+ * Problem: 
+ * Date: April 24th, 2020
+ * 
+ * 
+ * 
+ * Input:
+ * 
+ * Output:
+ * 
+ *******************************************************************/
+
+public class Day3 {
+
+	/************************************************************************
+	 * Solution:
+	 * 
+	 * 
+	 * 
+	 * Time complexity: 
+	 ************************************************************************/
+	
+	
+	
+	public static void problemTests() {
+		
+	}
+	
+	// Run problem tests
+	public static void main(String[] args) {
+		System.out.println("Running Tests:");
+		System.out.println("===========================\n");
+		problemTests();
+	}
+		
+	public static void test(int result, int expect) {
+		if (result == expect) { System.out.println("Correct"); }
+		else { System.out.println("Incorrect"); }
+		System.out.println("Output: " + result);
+		System.out.println("Expect: " + expect);
+		System.out.println("\n===========================\n");
+	}
+}
