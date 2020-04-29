e805acc3ade90c38a01f791fc7367171d54d9e01
Derek Chaplin <wolfborg@users.noreply.github.com>
2020-04-29 01:43:17 -0400

    Day8 online solution

--- src/april2020/Day8.java
+++ src/april2020/Day8.java
@@ -1,6 +1,6 @@
 package april2020;
 
-import java.util.Arrays;
+import java.util.stream.IntStream;
 
 public class Day8 {
 	/*******************************************************************
@@ -32,7 +32,8 @@ public class Day8 {
 	 * 4. Loop adds the current x value into the sum
 	 * 5. Returns 2 decimal point string
 	 * 
-	 * I feel like there's definitely a cleaner way to simplify this.
+	 * I feel like there's definitely a cleaner way to simplify this. Though
+	 * it looks like most solutions online are very similar to this.
 	 * 
 	 * Time complexity: O(N)
 	 ************************************************************************/
@@ -51,21 +52,35 @@ public class Day8 {
 	/************************************************************************
 	 * Online Solution
 	 * 
-	 * 1. 
+	 * New stuff:
+	 * IntStream.range(x,y) - Creates an IntStream range from int x to int y
+	 * IntStream.mapToDouble() - Returns DoubleStream results of a function
+	 * DoubleStream.sum() - Adds the stream together
 	 * 
+	 * 1. Create an IntStream using a range from 0 to n
+	 * 2. Convert the stream to a DoubleStream
+	 * 3. Apply the series function to the stream
+	 * 4. Add the stream together to get answer
+	 * 5. Return the answer as a 2 decimal String
 	 * 
+	 * A cleaner solution, but essentially does the same thing as mine. Seems
+	 * like streams are the way for a lot of clean solutions online. That and
+	 * learning how to apply functions as a parameter could improve skills.
 	 * 
-	 * Time complexity: 
+	 * Time complexity: O(N)
 	 ************************************************************************/
-	public static Object solution2(Object a) {
-		return a;
+	public static String solution2(int n) {
+		return String.format("%.2f",
+			IntStream.range(0, n)
+			.mapToDouble(num -> 1.0 / (1 + num * 3))
+			.sum());
 	}
 	
 	public static void problemTests() {
-		Test.test(solution(1), "1.00");
-		Test.test(solution(2), "1.25");
-		Test.test(solution(5), "1.57");
-		Test.test(solution(0), "0.00");
+		Test.test(solution2(1), "1.00");
+		Test.test(solution2(2), "1.25");
+		Test.test(solution2(5), "1.57");
+		Test.test(solution2(0), "0.00");
 	}
 	
 	
