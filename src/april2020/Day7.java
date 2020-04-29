5a185af156ee521dc2168ac99d74b27fd3a137df
Derek Chaplin <wolfborg@users.noreply.github.com>
2020-04-28 22:53:35 -0400

    Day7 solutions

--- src/april2020/Day7.java
+++ src/april2020/Day7.java
@@ -4,6 +4,7 @@ package april2020;
  * Problem: Find the unique number
  * Date: April 28th, 2020
  * 
+ * 
  * There is an array with some numbers. All numbers are equal except
  * for one. It's guaranteed that array contains at least 3 numbers.
  * 
@@ -13,33 +14,53 @@ package april2020;
  * 
  *******************************************************************/
 
+import java.util.Arrays;
 
 public class Day7 {
 	/************************************************************************
 	 * My Solution
 	 * 
-	 * 1. 
+	 * 1. Get the distinct elements in the array.
+	 * 2. Compare the first three elements.
+	 * 3. Return the unique value based on the comparison.
 	 * 
-	 * Time complexity: 
+	 * Basically, if the first element is equal to the second or third, then
+	 * we know the second distinct is the answer. Distinct is cleaner than a
+	 * for loop but it does still iterate over the whole array.
+	 * 
+	 * Time complexity: O(N)
 	 ************************************************************************/
-	public static double findUniq(double arr[]) {
-		return 0;
+	public static double findUniq(double[] arr) {
+		//System.out.println(Arrays.toString(arr));
+		double[] d = Arrays.stream(arr).distinct().toArray();
+		if (arr[0] == arr[1] || arr[0] == arr[2]) return d[1];
+		return d[0];
 	}
 	
 	/************************************************************************
 	 * Online Solution
 	 * 
-	 * 1. 
+	 * 1. Sort the array.
+	 * 2. If the first two doubles aren't equal, return first element.
+	 * 3. Otherwise, return the last element.
 	 * 
-	 * Time complexity: 
+	 * This solution seems easier than mine, but it's time complexity is
+	 * larger due to the sorting method. This means that it's actually worse
+	 * than looping over all elements of the array. It's a clever way to
+	 * solve the problem, but not an efficient way.
+	 * 
+	 * Time complexity: O(N*log(N))
 	 ************************************************************************/
-	public static double findUniq2(double arr[]) {
-		return 0;
+	public static double findUniq2(double[] arr) {
+		Arrays.sort(arr);
+	    return arr[0] == arr[1] ? arr[arr.length-1]:arr[0];
 	}
 	
 	public static void problemTests() {
-		test(findUniq(new double[] { 1, 1, 1, 2, 1, 1 }), 2);
-		test(findUniq(new double[] { 0, 0, 0.55, 0, 0 }), 0.55);
+		test(findUniq2(new double[] { 1, 1, 1, 2, 1, 1 }), 2.0);
+		test(findUniq2(new double[] { 0, 0, 0.55, 0, 0 }), 0.55);
+		test(findUniq2(new double[] { 0, 1, 1, 1, 1, 1, 1, 1 }), 0.0);
+		test(findUniq2(new double[] { 0, 1, 0 }), 1.0);
 	}
 	
 	public static boolean test(Object output, Object expect) {
